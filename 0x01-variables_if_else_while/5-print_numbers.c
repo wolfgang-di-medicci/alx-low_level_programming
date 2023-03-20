0747df848da2862da391a8f3948b4420ddb960ba
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-20 14:58:57 -0700

    for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - main entry point
+ * Return: 0 when successful
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf('\n');
+	return (0);
