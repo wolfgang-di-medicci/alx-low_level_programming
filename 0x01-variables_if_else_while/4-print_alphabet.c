8d88aae438e6e990678a90feb0a7a8c0da15fff6
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-20 14:39:06 -0700

    for task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - main entry point
+ * Return: 0 when succesful
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n')
+		return (0);
+}
+
+
