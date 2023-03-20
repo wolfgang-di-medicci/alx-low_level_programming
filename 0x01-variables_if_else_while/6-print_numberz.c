9256e3d5012466d16c81d15ed3f2b7461f35a94b
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-20 15:12:13 -0700

    for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - main entry point
+ * Return: 0 when finished
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+		return (0);
+}
