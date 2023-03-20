79e1dda338ae93ab97e3b9a34b80c463ccdf0bfe
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-20 13:59:17 -0700

    for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - main entry point
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
