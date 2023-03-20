212344ebb192af4e991db1c6fe1a37e0f3b504d2
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-20 15:34:36 -0700

    for task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - main entry point
+ * Return: 0 when succesful
+*/
+
+int main(void)
+{
+	char num;
+	char letter;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	for (letter = a; letter <= f; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
