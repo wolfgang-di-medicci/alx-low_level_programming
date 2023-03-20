e054dc81668e7727de570e2303e513d3acc2a289
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-20 15:24:01 -0700

    for task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - main entry point
+ * Return: 0 when succesful
+*/
+
+int main(void)
+{
+	char reva;
+
+	for (reva = 'z'; reva >= 'a'; reva--)
+	{
+		putchar(reva);
+	}
+	putchar('\n')
+		return (0);
+}
