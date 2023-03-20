0166668a8393c0d3356e21ef4b353fb1d94891a4
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-20 15:03:43 -0700

    for task 5 v.3

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	int x;
+	int num;
 
-	for (x = 0; x <= 9; x++)
+	for (num = 0; num <= 9; num++)
 	{
-		printf("%d", x);
+		printf("%d", num);
 	}
 	printf("\n");
 	return (0);
