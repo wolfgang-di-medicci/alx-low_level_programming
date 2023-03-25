d4c6dffb3fb9d1ea2fc5021541f260902974a44e
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-25 04:54:49 -0700

    for task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints to 98
+ * @n: variable
+ * Return: return 0 when successful
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	putchar('\n');
+}
+
