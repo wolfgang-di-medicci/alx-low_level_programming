9651db1f78c01fa09f5c4138f1999e101eae2e35
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-25 02:28:31 -0700

    for task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - main
+ * Return: from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int hr = 0, hrmax = 23, minut = 0, minutmax = 59;
+
+	while (hr <= hrmax)
+	{
+		while (minut <= minutmax)
+		{
+			_putchar(hr < 9 ? 0 + '0' : s / 10 + '0');
+			_putchar(hr < 9 ? hr + '0' : s % 10 + '0');
+			_putchar(':');
+			_putchar(minut < 9 ? 0 + '0' : minut / 10 + '0');
+			_putchar(minut < 9 ? minut + '0' : minut % 10 + '0');
+			_putchar('\n');
+			minute++;
+		}
+		minute = 0;
+		hr++;
+	}
