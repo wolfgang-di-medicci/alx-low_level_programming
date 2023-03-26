1fe23ec82e7d5015e7733b9818a4198e9fa41fa7
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-26 01:25:30 -0700

    for task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers without 2 and 4
+ * Return: nothing
+ */
+
+void print_most_numbers(void);
+{
+	int i;
+
+	for (i = 0, i <= 9, i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar( i + '0');
+		}
+	}
+	_putchar('\n');
+}
