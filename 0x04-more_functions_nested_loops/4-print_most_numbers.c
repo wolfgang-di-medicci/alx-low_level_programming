e871c2d7e55dad87990ee16100e8d8e2de240f94
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-26 01:30:00 -0700

    for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,11 @@
  * Return: nothing
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0, i <= 9, i++)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
 		{
