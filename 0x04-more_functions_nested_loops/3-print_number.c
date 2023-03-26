414cbc11bfd86322d13d314969e347d00c44c5a8
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-26 01:15:42 -0700

    for task 3

---
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * Return: 0 when succesful
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
