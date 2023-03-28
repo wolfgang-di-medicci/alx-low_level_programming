486c49e63074217c76cd9955a5024f4afb16c806
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-28 06:24:10 -0700

    for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse the input
+ * @s: input variable
+ * Return: return nothing
+ */
+
+void print_rev(char *s)
+{
+	int numstr = 0;
+
+	while (*s)
+	{
+		s++;
+		numstr++;
+	}
+	while (numstr)
+	{
+		s--;
+		_putchar(*s);
+		numstr--;
+	}
+}
