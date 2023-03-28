a8c7b3a409d1cd6d75772e50d081fd61e372d6ab
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-28 06:50:13 -0700

    for task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a given string
+ * @s: input variable string
+ * Return: when succesful nothing
+ */
+
+void rev_string(char *s)
+{
+	int c = 0;
+	int i = 0;
+	char last_char;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+	for (i = 0; i < c; i++)
+	{
+		s--;
+	}
+	for (i = 0; i < (c / 2); i++)
+	{
+		int last = c - i;
+
+		last_char = s[last - i];
+		s[last - i] = s[i];
+		s[i] = last_char;
+	}
+}
+
