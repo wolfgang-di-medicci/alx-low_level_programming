29081afeba04af35171cd2b5389fe94ecbc598ac
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-28 07:08:02 -0700

    for task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string variable
+ * Return: nothing on success
+ */
+
+void puts2(char *str)
+{
+	int c = 0, i;
+
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+	for (i = 0; i < c; i++)
+		str--;
+	for (i = 0; i < c; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
