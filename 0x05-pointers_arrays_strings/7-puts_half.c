084faa510887071a43c7190ad2b35c2b1f100eee
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-28 19:23:52 -0700

    for task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string varable
+ * Return: nothing when succesful
+ */
+
+void puts_half(char *str)
+{
+	int c = 0, i;
+	
+	while (*str)
+	{
+		c++;
+		str++;
+	}
+	for (i = 0; i < c; i++)
+	{
+		str--;
+	}
+	i = (c % 2 ==0) ? c / 2 : (c + 1) / 2;
+
+	for (; i < c; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
