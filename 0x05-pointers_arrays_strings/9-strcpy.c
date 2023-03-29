515c10e564a89b55cf5c756593674c3dd0c4ee18
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-03-28 20:00:36 -0700

    for task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string
+ * @dest: final destination of string
+ * @src: string been copied
+ * Return: the copied string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0, i;
+
+	while ( *src)
+	{
+		src++;
+		count;
+	}
+	for (i = 0; i < count; i++)
+
+		src--;
+	for (i = 0; i < count; i++)
+	{
+		dest[i] = *src;
+		src++;
+	}
+	dest[i] = *src;
+	return (dest);
+}
