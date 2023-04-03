937d6dcce47f5243433109b2ee157abc300f2b38
kaleb yohannese <kalebyohannese1234mm@gmail.com>
2023-04-03 14:58:42 -0700

    for task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry function
+ * @s: input array
+ * @accept: another input
+ * Return: 0 when succesful
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
