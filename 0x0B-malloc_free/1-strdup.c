bf48f70ae107c0b21b6b38a2f0dd09a6bf3d8f86
Kaleb Yohannese Tewelde <kalebyohannese1234mm@gmail.com>
2023-04-12 00:48:10 +0300

    for task 1
    betty done on vim of vs code wsl ubuntu terminal

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <main.h>
+
+/**
+ * _strdup - makes a duplicate of str to new memory allocation
+ * @str: string input
+ * Return: the duplicate string
+ */
+
+char *_strdup(char *str)
+{
+	char *strnew;
+	int i = 0, r = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		i++
+	}
+	strnew = malloc(sizeof(char) * (i + 1));
+	if (strnew == NULL)
+	{
+		return (NULL);
+	}
+	for (r = 0; str[r]; r++)
+	{
+		strnew[r] = str[r];
+	}
+	return (strnew);
+}
