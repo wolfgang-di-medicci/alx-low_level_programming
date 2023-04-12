b44bd164a2854d158928c8451fac4e6705c7f22a
Kaleb Yohannese Tewelde <kalebyohannese1234mm@gmail.com>
2023-04-13 01:50:05 +0300

    for task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into the file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 when succesful
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
