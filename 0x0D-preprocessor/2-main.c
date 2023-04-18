ffd99060c56c041c3daf586187c9b08e1dfb81dd
Kaleb Yohannese Tewelde <kalebyohannese1234mm@gmail.com>
2023-04-19 00:00:17 +0300

    for task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the filename from which this function is compiled
+ * Return: when succesful 0
+*/
+
+int main(void)
+{
+	int i;
+	const char* filename = __FILE__;
+	
+	for (i = 0; filename[i] != '\0'; i++)
+	{
+		_putchar(filename[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
