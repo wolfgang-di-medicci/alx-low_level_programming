21961132999b02332a9a4c8851932360821fbfbf
Kaleb Yohannese Tewelde <kalebyohannese1234mm@gmail.com>
2023-04-19 02:10:27 +0300

    for task 3 on task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog for task 3
+*/
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
