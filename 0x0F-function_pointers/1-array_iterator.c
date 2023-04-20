6d51331f8e51070af7d4e90f403779dde244d033
Kaleb Yohannese Tewelde <kalebyohannese1234mm@gmail.com>
2023-04-20 20:45:47 +0300

    for task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - prints each array element on a newline
+ * @array: array input
+ * @size: size of array
+ * @action: pointer to a fuction that performs specific task
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
