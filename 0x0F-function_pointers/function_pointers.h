78d778db8b89c4deb7cc8fd0ea23bf766bbd31ac
Kaleb Yohannese Tewelde <kalebyohannese1234mm@gmail.com>
2023-04-20 20:03:31 +0300

    trying header file with small letters

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef function_pointers_h
+#define function_pointers_h
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
